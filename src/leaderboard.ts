3921e23d52a755aee38f1683c7a7efbee04bc455
Asad Nazir <asadnazir@MS1447-AsadNazir.local>
2024-07-25 19:25:48 +0500

    LeaderBoard Added

---
+++ src/leaderboard.ts
@@ -0,0 +1,7 @@
+let getLeaderboardRPC:nkruntime.RpcFunction = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, payload: string): string {
+    
+   // nk.leaderboardCreate()
+    return JSON.stringify({
+
+    })
+}
--- src/main.ts
+++ src/main.ts
@@ -1,62 +1,3 @@
-interface StorageResponse
-{
-    message:string,
-    status:string,
-    response:any
-}
-
-
-let helloWorldrpc: nkruntime.RpcFunction = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, payload: string): string {
-    return JSON.stringify(
-        {
-            "message": "This is a hello world RPC"
-        }
-    )
-}
-
-function setDefaultUserState()
-{
-
-}
-
-let writeUserStateRPC: nkruntime.RpcFunction = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, payload: string): string {
-    let payloadJson = JSON.parse(payload);
-
-    //Creating an instance of Storage Utility
-    let S: StorageUtility = new StorageUtility();
-
-    //Writing to the Collection and Returning a response
-    let res = '';
-
-    //read request
-    // var objectId: nkruntime.StorageReadRequest = {
-    //     collection: collectionName,
-    //     key: 'daily',
-    //     userId: ctx.userId,
-    // }
-
-    // var objects: nkruntime.StorageObject[];
-    // try {
-    //     objects = nk.storageRead([objectId]);
-    // } catch (error) {
-    //     logger.error('storageRead error: %s', error);
-    //     throw error;
-    // }
-
-    let _response:StorageResponse = 
-    {
-        message:"Data Written",
-        status:"Done",
-        response:res
-
-    }
-
-    return JSON.stringify(
-        _response
-    )
-}
-
-
 
 let InitModule: nkruntime.InitModule =
     function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, initializer: nkruntime.Initializer) {
@@ -64,8 +5,6 @@ let InitModule: nkruntime.InitModule =
 
         // let Auth:AuthRPC = new AuthRPC();
 
-        initializer.registerRpc("Hello World", helloWorldrpc);
         initializer.registerRpc("CutsomAuth", customAuthRpc);
-        initializer.registerRpc("WriteUserStateRPC",writeUserStateRPC);
 
     }
