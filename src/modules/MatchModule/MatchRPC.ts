aa6fc048f4cc1576cd93c2a9919d72ae08fdc134
Asad Nazir <asadnazir@MS1447-AsadNazir.local>
2024-07-29 13:32:22 +0500

    Code Refactored again

--- src/modules/MatchModule/MatchRPC.ts
+++ src/modules/MatchModule/MatchRPC.ts
@@ -13,7 +13,7 @@ let matchEnd: nkruntime.RpcFunction = function (ctx: nkruntime.Context, logger: 
 
         //Calculating score
         const score:number = matchObj.scoreCalculator(newUserState.level,newUserState.xp);
-        const response = nk.leaderboardRecordWrite(Constants.LEADBOARD_NAME,ctx.userId,ctx.username,newUserState.level);
+        const response = nk.leaderboardRecordWrite(Constants.LEADBOARD_NAME,ctx.userId,ctx.username,score);
 
         //updating user state 
         const userStateObj:State = new State();
--- src/modules/MatchModule/MatchStorage.ts
+++ src/modules/MatchModule/MatchStorage.ts
@@ -1,20 +1,9 @@
 class MatchStorage
 {
     public scoreCalculator(level:number, xp:number):number {
-            // Define maximum values for normalization (adjust as needed)
-            const maxLevel = 100;
-            const maxXp = 10000;
-        
-            // Define weight for XP contribution (adjust as needed)
-            const weight = 0.5;
-            
-            // Normalize level and XP
-            const normalizedLevel = level/ maxLevel;
-            const normalizedXp = xp / maxXp;
             
             // Calculate score using the weighted sum
-            const score = normalizedLevel + weight * normalizedXp;
-            
+            const score:number =  level * xp;
             return score;
         }
 }
